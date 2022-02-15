52c71b1f224f0e7e0cbb7775602cdcbf9d8d3dbc
kayla <kay.ann2000@gmail.com>
2022-02-15 10:44:02 -0700

    commit tree c477bffc4a1ff972dc3828102bcd7b8e02cb1326

---
+++ index.js
@@ -0,0 +1,71 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) { return a - b; };
+
+function multiply(a, b) { return a * b; };
+
+
+function divide(a, b) { return a / b; };
+function increment(n) { return n+1; }
+function decrement(n) { return n-1; }
+function makeInt(n) {};
+
+function preserveDecimal(n) {};
+
+
+function makeInt(intString) {
+    return parseInt(intString, 10)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function  preserveDecimal(floatAsString) {
+    return parseFloat(floatAsString)
+}
+
+
+
+
+
+
+
